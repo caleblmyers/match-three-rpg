9436dc69f0013612f68753e6afe0aadb9812678e
Caleb Myers <cmyers5108@gmail.com>
2019-08-04 21:06:40 -0400

    storing

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,29 +5,44 @@
 // [4, "Gold"],
 // [5, "Purple"]];
 
-var tileTypes = [0, 1, 2, 3, 4, 5]
+var tileTypes = [0, 1, 2, 3, 4, 5];
 
 function Level() {
-  this.col = 8;
-  this.row = 8;
+  this.rows = 8;
+  this.columns = 8;
   this.tileArray = [];
 };
 
-function Tile() {
-  this.type = tileTypes[Math.floor(Math.random() * tileTypes.length)];
+function Tile(i, j) {
+  this.type = Math.floor(Math.random() * tileTypes.length);
+  this.row = i;
+  this.col = j;
 };
 
 Level.prototype.init = function () {
-  for (let i = 0; i < level.col; i++) {
-    this.tileArray[i] = []
-    for (let j = 0; j < level.row; j++) {
-      this.tileArray[i][j] = new Tile()
+  for (let i = 0; i < level.rows; i++) {
+    this.tileArray[i] = [];
+    for (let j = 0; j < level.columns; j++) {
+      this.tileArray[i][j] = new Tile(i, j);
+    };
+  };
+};
+
+Level.prototype.display = function () {
+  for (let i = 0; i < level.rows; i++) {
+    // var tileRow = ''
+    for (let j = 0; j < level.columns; j++) {
+      process.stdout.write(this.tileArray[i][j].type.toString())
+      // tileRow += this.tileArray[i][j].type + "  "
     }
+    process.stdout.write("\n")
+    // console.log(tileRow)
   }
 }
 
 var level = new Level();
 
-level.init()
-
-console.log(level.tileArray)
+level.init();
+level.display()
+// console.log(level.tileArray);
+// console.log(level.tileArray[3][4]);
